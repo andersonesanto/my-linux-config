10f7765743c91ebe89389b5b20bf41273222a68c
Anderson Santo <anderson.santo@mv.com.br>
2021-06-29 09:11:15 -0300

    updates

--- apply_configs.sh
+++ apply_configs.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-if [ ! -L "$HOME/.vimrc" ];  then
-    cp $HOME/.vimrc $HOME/.vimrc.bak
-    rm $HOME/.vimrc -f
-    ln -s $HOME/santo_configs/.vimrc $HOME/.vimrc
-fi
+rm $HOME/.vimrc -f
+ln -s $HOME/santo_configs/_vimrc $HOME/.vimrc
 
+rm $HOME/.zprofile -f
+ln -s $HOME/santo_configs/_zprofile $HOME/.zprofile
 
-if [ ! -L "$HOME/.zprofile" ];  then
-    cp $HOME/.zprofile $HOME/.zprofile.bak
-    rm $HOME/.zprofile -f
-    ln -s $HOME/santo_configs/.zprofile $HOME/.zprofile
-fi
-
-if [ ! -L "$HOME/.zshrc" ];  then
-    cp $HOME/.zshrc $HOME/.zshrc.bak
-    rm $HOME/.zshrc -f
-    ln -s $HOME/santo_configs/.zshrc $HOME/.zshrc
-fi
+rm $HOME/.zshrc -f
+ln -s $HOME/santo_configs/_zshrc $HOME/.zshrc
