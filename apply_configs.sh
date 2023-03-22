703b5579935a2928c1af3c8fb583c1e5bb56d1e2
Anderson E Santo <86746798+andersonesanto@users.noreply.github.com>
2023-03-22 19:10:02 -0300

    Update apply_configs.sh

--- apply_configs.sh
+++ apply_configs.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-cd ~/santo_configs
+cd ~/my-linux-config
 git pull
 
 ## Copia o tema do oh-my-zsh para o local correto
 cp fox2.zsh-theme ~/.oh-my-zsh/themes/fox2.zsh-theme -f
 
 rm $HOME/.vimrc -f
-ln -s $HOME/santo_configs/_vimrc $HOME/.vimrc
+ln -s $HOME/my-linux-config/_vimrc $HOME/.vimrc
 
 rm $HOME/.zprofile -f
-ln -s $HOME/santo_configs/_zprofile $HOME/.zprofile
+ln -s $HOME/my-linux-config/_zprofile $HOME/.zprofile
 
 rm $HOME/.zshrc -f
-ln -s $HOME/santo_configs/_zshrc $HOME/.zshrc
+ln -s $HOME/my-linux-config/_zshrc $HOME/.zshrc
 
 rm $HOME/.tmux.conf -f
-ln -s $HOME/santo_configs/_tmuxconf $HOME/.tmux.conf
+ln -s $HOME/my-linux-config/_tmuxconf $HOME/.tmux.conf
 
 cd
 
